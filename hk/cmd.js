abef9b8747c16f92a2fbae271bc888c66b21d301
eng-dibo <sh.eldeeb.2010@gmail.com>
2020-07-08 23:06:01 +0200

    ..

--- hk/cmd.js
+++ hk/cmd.js
@@ -1,16 +1,21 @@
 {
-  example:function(){
+  notes :function(){
    /*
    this file must be json-like (but you can use functions and js features),
    don't use comments, or add ';' to the end of the data.
 
    use `GM` constant to access GM scope. (GM stands for greasemonkey)
    to get info (dev, user, userGroup, script, ...), use `let info = GM.getInfo();`
+    */
+
+   /*
+   todo:
+   google_8720: function() {
+     GM.GM_openInTab("https://accounts.google.com/AddSession")
+   }
 
     */
   },
-  google_8720_2: function() {
-    GM.GM_openInTab("https://accounts.google.com/AddSession")
-  }
+
 
 }
